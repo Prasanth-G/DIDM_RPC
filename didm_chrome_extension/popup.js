6cac6ba5e23cf03773eb1df8e1abaaa5cbadc644
prasanth-g <prasanthganesamoorthy@outlook.com>
2019-04-12 12:42:54 +0530

    chrome extension prototype

---
+++ didm_chrome_extension/popup.js
@@ -0,0 +1,21 @@
+//let changeColor = document.getElementById('changeColor');
+
+//chrome.storage.sync.get('color', function(data) {
+  //changeColor.style.backgroundColor = data.color;
+  //changeColor.setAttribute('value', data.color);
+//});
+
+startDownload.onclick = function(element) {
+  //RPC to local client with the current settings
+  //RPC params : download link, settings
+  
+
+
+  // let color = element.target.value;
+  // chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+  //   chrome.tabs.executeScript(
+  //       tabs[0].id,
+  //       {code: 'document.body.style.backgroundColor = "' + color + '";'});
+  // });
+  
+};
